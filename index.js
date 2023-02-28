5e0585a3f2cb212685c5aa4c8944ed27bd8bb8d7
Emi Jerochim <emi.jerochim@hotmail.com>
2023-02-28 01:36:11 -0300

    logs

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ discord.on("messageCreate", async (message) => {
         top_p: 0.7,
         frequency_penalty: 0.7,
         presence_penalty: 0.7,
-        prompt: message.content.slice(7),
+        prompt: prompts.debug,
       });
       await message.reply(response.data.choices[0].text);
     }
